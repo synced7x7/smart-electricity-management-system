e18af6cee038031364f7bfa06d60baef7ac91d9d
fahim-tajoar <fahimpramanik@iut-dhaka.edu>
2026-08-23 17:54:55 +0600

    test(notification): add comprehensive unit and integration test suite

--- notification-service/src/test/java/com/desco/notification/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/com/desco/notification/NotificationServiceApplicationTests.java
@@ -1,13 +1,14 @@
-package com.desco.notificationservice;
+package com.desco.notification;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class NotificationServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
